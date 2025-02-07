930e465a555bf2e5da91fe9d3a8356966be0899d
tpantsar <tomi.pantsar@gmail.com>
2025-02-07 13:47:00 +0200

    Test that user can navigate from the main page to the page of a particular Pokemon

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -8,4 +8,16 @@ describe('Pokedex', () => {
       page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')
     ).toBeVisible()
   })
+
+  test('navigate from the main page to the page of ivysaur Pokemon', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await page.click('text="ivysaur"')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('previous')).toBeVisible()
+    await expect(page.getByText('home')).toBeVisible()
+    await expect(page.getByText('next')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await expect(page.url()).toBe('http://127.0.0.1:8080/pokemon/ivysaur')
+  })
 })
